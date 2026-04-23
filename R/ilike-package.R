fa13d3afc6eb6cde96fad99e4b4863648a8839aa
Richard Everitt <richard.g.everitt@gmail.com>
2026-04-23 12:08:19 +0100

    Add precompiled header support and pkg updates
    Add a precompiled header (src/pch.h) and update src/Makevars and src/Makevars.win to build and use pch.h.gch (PKG_CXXFLAGS += -include pch.h). Ignore generated .gch files via .Rbuildignore and src/.gitignore. Replace package roxygen block in R/ilike-package.R with _PACKAGE and remove the generated man/ilike.Rd documentation file. Also bump RoxygenNote in DESCRIPTION.

--- R/ilike-package.R
+++ R/ilike-package.R
@@ -1,15 +1,6 @@
-#' ilike: Methods for Bayesian computation when the likelihood is intractable.
-#'
-#' It contains some really useful stuff.
-#'
-#' @section Functions:
-#' Some functions.
-#'
-#' @docType package
-#' @name ilike
-#'
+"_PACKAGE"
+
 #' @useDynLib ilike, .registration = TRUE
-#'
 #' @importFrom Rcpp sourceCpp
 #' @importFrom RcppParallel RcppParallelLibs
 NULL
