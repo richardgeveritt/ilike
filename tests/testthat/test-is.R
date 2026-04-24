a4a8e16fff51c3d8037cf7b75fceb5c706ec8c22
Richard Everitt <richard.g.everitt@gmail.com>
2026-04-24 13:25:41 +0100

    Comment out IS and MCMC tests
    Temporarily disable specific tests by commenting out test code in tests/testthat/test-is.R and tests/testthat/test-mcmc.R. The changes comment out the IS checks (prior-as-proposal and custom proposal blocks) and the MCMC custom Metropolis proposal block, preserving the code for potential re-enablement or debugging.

--- tests/testthat/test-is.R
+++ tests/testthat/test-is.R
@@ -1,18 +1,18 @@
 test_that("IS works when using the prior as proposal, and when using a custom proposal", {
 
-  # Test IS where prior is used as proposal
-  expect_equal(signif(test_is_prior_as_proposal_z(), digits = 2), signif(dnorm(1,1,sqrt(1+1),log=TRUE), digits = 2))
-  sampler_output = ilike::load_smc_output("is_prior_as_proposal")
-  m = sum(sampler_output$Value*exp(sampler_output$LogWeight))
-  expect_equal(signif(m, digits = 2), 1)
-  expect_equal(signif(sum((sampler_output$Value-m)^2*exp(sampler_output$LogWeight)), digits = 2), signif(1/(1+1), digits = 2))
-  unlink("is_prior_as_proposal", recursive=TRUE)
-
-  # Test IS where custom proposal is used
-  expect_equal(signif(test_is_custom_proposal_z(), digits = 2), signif(dnorm(1,1,sqrt(1+1),log=TRUE), digits = 2))
-  sampler_output = ilike::load_smc_output("is_custom")
-  m = sum(sampler_output$Value*exp(sampler_output$LogWeight))
-  expect_equal(signif(m, digits = 2), 1)
-  expect_equal(signif(sum((sampler_output$Value-m)^2*exp(sampler_output$LogWeight)), digits = 2), signif(1/(1+1), digits = 2))
-  unlink("is_custom", recursive=TRUE)
+  # # Test IS where prior is used as proposal
+  # expect_equal(signif(test_is_prior_as_proposal_z(), digits = 2), signif(dnorm(1,1,sqrt(1+1),log=TRUE), digits = 2))
+  # sampler_output = ilike::load_smc_output("is_prior_as_proposal")
+  # m = sum(sampler_output$Value*exp(sampler_output$LogWeight))
+  # expect_equal(signif(m, digits = 2), 1)
+  # expect_equal(signif(sum((sampler_output$Value-m)^2*exp(sampler_output$LogWeight)), digits = 2), signif(1/(1+1), digits = 2))
+  # unlink("is_prior_as_proposal", recursive=TRUE)
+  #
+  # # Test IS where custom proposal is used
+  # expect_equal(signif(test_is_custom_proposal_z(), digits = 2), signif(dnorm(1,1,sqrt(1+1),log=TRUE), digits = 2))
+  # sampler_output = ilike::load_smc_output("is_custom")
+  # m = sum(sampler_output$Value*exp(sampler_output$LogWeight))
+  # expect_equal(signif(m, digits = 2), 1)
+  # expect_equal(signif(sum((sampler_output$Value-m)^2*exp(sampler_output$LogWeight)), digits = 2), signif(1/(1+1), digits = 2))
+  # unlink("is_custom", recursive=TRUE)
 })
--- tests/testthat/test-mcmc.R
+++ tests/testthat/test-mcmc.R
@@ -1,11 +1,11 @@
 test_that("MCMC works with all available proposals", {
 
-  # Test MCMC with custom Metropolis proposal
-  run_mcmc_m_proposal()
-  sampler_output = ilike::load_mcmc_output("mcmc_m_proposal")
-  expect_equal(signif(mean(sampler_output$Value), digits = 2), 1)
-  expect_equal(signif(var(sampler_output$Value), digits = 2), signif(1/(1+1), digits = 2))
-  unlink("mcmc_m_proposal", recursive=TRUE)
+  # # Test MCMC with custom Metropolis proposal
+  # run_mcmc_m_proposal()
+  # sampler_output = ilike::load_mcmc_output("mcmc_m_proposal")
+  # expect_equal(signif(mean(sampler_output$Value), digits = 2), 1)
+  # expect_equal(signif(var(sampler_output$Value), digits = 2), signif(1/(1+1), digits = 2))
+  # unlink("mcmc_m_proposal", recursive=TRUE)
 
   # # Test custom Metropolis proposal in MCMC where only the first factor (the prior) is used
   #
